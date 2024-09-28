f394db6dcbad05bca7b38644bdc2241bb2d6a368
LtECoD <jensus.yang@hotmail.com>
2024-09-28 10:27:09 +0800

    continue train on a800

--- train_test.sh
+++ train_test.sh
@@ -1,10 +1,38 @@
 #! /bin/bash
 
+#SBATCH --job-name mnode_test
+#SBATCH -p gpu
+#SBATCH -N 2
+#SBATCH --ntasks-per-node 1
+#SBATCH --gpus-per-node 4                   
+#SBATCH --cpus-per-task 4
+#SBATCH --gpu-bind=none
+#SBATCH --output ./test/test.out
+#SBATCH --error ./test/test.err
+#SBATCH --exclusive
+
 eval "$(conda shell.bash hook)"
 conda activate esm3
 
-export NPROC_PER_NODE=2
+export NPROC_PER_NODE=4
 export OMP_NUM_THREADS=8
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export NCCL_DEBUG=INFO
 
-torchrun --nproc_per_node $NPROC_PER_NODE \
+echo ${MASTER_ADDR}:${MASTER_PORT}
+echo $SLURM_NODEID
+
+srun torchrun \
+    --nnodes ${SLURM_NNODES} \
+    --nproc_per_node $NPROC_PER_NODE \
+    --rdzv_id $RANDOM \
+    --rdzv_backend c10d \
+    --rdzv_endpoint ${MASTER_ADDR}:${MASTER_PORT} \
     source/train.py config/test.yaml
+
+
+
+    # --node_rank=$SLURM_NODEID \
+    # --master_addr=$MASTER_ADDR \
+    # --master_port=$MASTER_PORT \
--- train_th.sh
+++ train_th.sh
@@ -1,11 +1,34 @@
 #! /bin/bash
 
+#SBATCH --job-name tune
+#SBATCH -p gpu
+#SBATCH -N 2
+#SBATCH --ntasks-per-node 1
+#SBATCH --gpus-per-node 4                   
+#SBATCH --cpus-per-task 4
+#SBATCH --gpu-bind=none
+#SBATCH --output ./log/coflow_continue.out
+#SBATCH --error ./log/coflow_continue.err
+#SBATCH --exclusive
+
+module load cuda/12.4
 eval "$(conda shell.bash hook)"
 conda activate esm3
 
-export NPROC_PER_NODE=8
+export NPROC_PER_NODE=4
 export OMP_NUM_THREADS=8
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export NCCL_DEBUG=INFO
 
-torchrun --nproc_per_node $NPROC_PER_NODE \
-    source/train.py config/finetune.yaml
+echo ${MASTER_ADDR}:${MASTER_PORT}
+echo $SLURM_NODEID
+
+srun torchrun \
+    --nnodes ${SLURM_NNODES} \
+    --nproc_per_node $NPROC_PER_NODE \
+    --rdzv_id $RANDOM \
+    --rdzv_backend c10d \
+    --rdzv_endpoint ${MASTER_ADDR}:${MASTER_PORT} \
+    source/train.py config/finetune_continue.yaml
 
